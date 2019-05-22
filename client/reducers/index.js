5eb6c54eb82efa05b217db72cba1d19ba433109a
Alyona <alyona.rodin@gmail.com>
2019-05-22 16:44:32 -0400

    combined  reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,17 +1,21 @@
 /* combineReducers is not currently used, but eventually should be for modular code :D */
 import { combineReducers } from 'redux'
 import  { countriesReducer } from './countriesReducer'
+import {countryReducer} from  './countryReducer'
+import {aircraftReducer} from './aircraftReducer'
+import {aircraftsReducer} from './aircraftsReducer'
 
 // const initialState = {}
-
 // const rootReducer = function(state = initialState, action) {
 //   switch(action.type) {
 //     default: return state
 //   }
 // };
-
 // export default rootReducer
 
 export default combineReducers({
-  countriesReducer
+  countriesReducer,
+  countryReducer,
+  aircraftReducer,
+  aircraftsReducer
 })
